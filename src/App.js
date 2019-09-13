c4fabf8f308bf9ef0954315c1dc69d6e9294ec39
Ronaldo David Gomes Gligan <ronaldodavidgomesgligan@gmail.com>
2019-09-13 11:03:58 +0200

    Changing Romanian Description

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
           </p>
           <p className="left-indicator">
             <h2 className="txt">Language Skills</h2>
-            I speak <i>very well</i> <b>English</b> 🇺🇸 from many years ago 😊. También hablo <b>Español</b> 🇪🇸 (<i>Castellano</i>) a nivel nativo, <i>i també parle una mica de <b>Valencià</b></i> 🥘. Știu ceva despre limba <b>Română</b> 🇷🇴.
+            I speak <i>very well</i> <b>English</b> 🇺🇸 from many years ago 😊. También hablo <b>Español</b> 🇪🇸 (<i>Castellano</i>) a nivel nativo, <i>i també parle una mica de <b>Valencià</b></i> 🥘. In plus, știu să vorbesc <b>Română</b> 🇷🇴 și să dialoghez în această limbă.
             <br /><br />J'apprends le <b>Français</b> 🇫🇷 и еще я учу <b>русский</b> 🇷🇺 язык, мне нравится, но мне трудно.
           </p>
           <p className="left-indicator">
